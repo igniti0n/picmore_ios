1049956c5bf9bc0e2bfe670e4346d3b9ad0a4058
Ivan Štajcer <istajcer@etfos.hr>
2022-07-31 16:54:50 +0200

    Initial Commit

---
+++ picmore_ios/picmore_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  picmore_iosApp.swift
+//  picmore_ios
+//
+//  Created by Ivan Stajcer on 31.07.2022..
+//
+
+import SwiftUI
+
+@main
+struct picmore_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
